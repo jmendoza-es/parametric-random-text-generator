eb40495cab0f1f54cd68290cc96852770e722504
Javier Mendoza <info@jmendoza.es>
2024-01-12 04:59:44 +0100

    fix(): code cleanup

--- index.js
+++ index.js
@@ -1,111 +1,86 @@
 function getRandomText() {
-	
+
   var phs = '';
   var wds = '';
   var wd = '';
   var words = arguments[0];
-  var phrases = (arguments[1]) ? arguments[1] : 1;
-  var syllables = (arguments[2]) ? arguments[2] : 4;
-  var prefix = (arguments[3]) ? arguments[3] : '';
-  var suffix = (arguments[4]) ? arguments[4] : '';
-  
+  var phrases = arguments[1] || 1;
+  var syllables = arguments[2] || 4;
+  var prefix = arguments[3] || '';
+  var suffix = arguments[4] || '';
+
   // Possible characters
-  this.t = ['gü'];
-  this.s = ['h'];
-  this.d = ['rr','cs','bs','ñ'];
-  this.w = ['i','e'];
-  this.v = ['a','e','i','o','u'];
-  this.x = ['á','é','í','ó','ú'];
-  this.c = ['b','d','f','g','j','l','m','n','p','r','s','t','v','z'];
+  var characters = {
+    t: ['gü'],
+    s: ['h'],
+    d: ['rr', 'cs', 'bs', 'ñ'],
+    w: ['i', 'e'],
+    v: ['a', 'e', 'i', 'o', 'u'],
+    x: ['á', 'é', 'í', 'ó', 'ú'],
+    c: ['b', 'd', 'f', 'g', 'j', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'z']
+  };
 
-	// Possible combinations
-  let m =  [
-    {
-      algorythm: 'cv',
-      coeficient: 0.85
-    },          
-    {
-      algorythm: 'cw',
-      coeficient: 0.85
-    },      
-    {
-      algorythm: 'sv',
-      coeficient: 0.85
-    },
-    {
-      algorythm: 'dv',
-      coeficient: 0.85
-    },
-    {
-      algorythm: 'tw',
-      coeficient: 0.85
-    }
+  // Possible combinations
+  var algorithms = [
+    { algorithm: 'cv', coefficient: 0.85 },
+    { algorithm: 'cw', coefficient: 0.85 },
+    { algorithm: 'sv', coefficient: 0.85 },
+    { algorithm: 'dv', coefficient: 0.85 },
+    { algorithm: 'tw', coefficient: 0.85 }
   ];
-  
-  var incompatibleAsFirstFragment = ['d','t','s'];
-    
-  function rand(max, min = 0) {
-  	return Math.floor(Math.random() * max) + min;
-  }
-  
-  function fragment() {
 
-		var i;
-    var p = m[rand(m.length)];
-    var t = p;
-    t = t.algorythm.split('');
+  var incompatibleAsFirstFragment = ['d', 't', 's'];
+
+  function rand(max, min = 0) {
+    return Math.floor(Math.random() * max) + min;
+  }
+
+  function fragment() {
+    var p = algorithms[rand(algorithms.length)];
+    var t = p.algorithm.split('');
     var o = '';
-    
-    for( i = 0 ; i < t.length; i++ ) {  
-      let h = t[i];   
-      
-      if(incompatibleAsFirstFragment.includes(h) && window.e == 0) {
-       	h = 'c';
-      } else {
-       	h = t[i];
+
+    for (var i = 0; i < t.length; i++) {
+      var h = t[i];
+
+      if (incompatibleAsFirstFragment.includes(h) && wd === '') {
+        h = 'c';
       }
-      
-      let fr = this[h][rand(this[h].length)];
-      o += this[h][rand(this[h].length)];
-      
+
+      o += characters[h][rand(characters[h].length)];
     }
 
     return o;
   }
-  
-	function word() {
-    for( e = 0 ; e < rand(syllables) ; e++ ) {
+
+  function word() {
+    for (var e = 0; e < syllables; e++) {
       wd += fragment().trim();
     }
-		
-		if(wd.charAt(0) == '') {
-    	return word();
-    };
-    
+
+    if (wd === '') {
+      return word();
+    }
+
     return wd;
   }
-  
-  function phrase() {
-    for( a = 0 ; a < words ; a++ ) {  
-    	wds += (!a) ? '' : ' ';  
-			wds += word();
-      wd = '';
-    } 
-    return prefix + wds.charAt(0).toUpperCase() + wds.slice(1) + suffix;
 
+  function phrase() {
+    for (var a = 0; a < words; a++) {
+      wds += (a === 0) ? '' : ' ';
+      wds += word();
+      wd = '';
+    }
+    return prefix + wds.charAt(0).toUpperCase() + wds.slice(1) + suffix;
   }
-  
-  for( b = 0 ; b < phrases ; b++ ) {
+
+  for (var b = 0; b < phrases; b++) {
     phs += phrase();
     wds = '';
   }
-  
-  return phs;
 
+  return phs;
 }
 
 // Words - Phrases - Max syllables - Prefix - Suffix
-document.write('<ol>' + getRandomText(5, 15, 5, '<li>', '.</li>') + '</ol>'); 
-
-
-
+document.write('<ol>' + getRandomText(5, 15, 5, '<li>', '.</li>') + '</ol>');
